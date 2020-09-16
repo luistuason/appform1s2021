c33957e9bde7a45cd931708efe2125c100e4b752
Luis Tuason <philipluistuason@gmail.com>
2020-09-16 21:43:40 +0800

    form working

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 $(document).ready(function(){
-  $('#scene2').hide()
+  $('#scene2').hide();
 
   $('#scene1').click(function(){
       $('#scene1').fadeOut(800, function(){
           $('#scene2').fadeIn(800);
+          $('#welcome-img').fadeOut(800);
       });
 
+  });
+
+  $("form[name='registration']").validate({
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+    },
+    // Specify validation error messages
+    messages: {
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
   });
 });
